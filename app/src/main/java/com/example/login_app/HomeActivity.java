3a1e798b28be36d03c0d67ce4fe33f59c7faaec3
sayedBME <bmi1715028@gmail.com>
2023-07-27 14:16:29 +0600

    Login App

---
+++ app/src/main/java/com/example/login_app/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.login_app;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    Button logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
+            }
+        });}}
